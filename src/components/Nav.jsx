e43d1d6a264ab81e1fbb3ac3ae3a62d231dbcecd
phoenixjmh <phoenixjmh@gmail.com>
2023-05-27 22:53:20 -0600

    Added home button back to nav bar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,13 +1,19 @@
 import { ReactComponent as CartIcon } from "../icons/cart.svg";
-
+import { Link } from "react-router-dom";
 const Nav = ({ cart, openCart }) => {
   return (
     <nav>
+      <Link to ='/'>
+        <h2 id="nav-title">
+          TONE-DEPOT
+        </h2>
+      </Link>
       <h2 onClick={openCart}>
         <CartIcon />
         <div className="cart-badge">{cart.length}</div>
       </h2>
     </nav>
+    
   );
 };
 
